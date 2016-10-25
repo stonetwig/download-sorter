9c3e97f40ede224df731e72143607e0f7202489a
Markus Stenqvist <daysnine@gmail.com>
2016-10-25 22:02:41 +0200

    Improving

--- download-sorter.ps1
+++ download-sorter.ps1
@@ -1,2 +1,27 @@
 # UBER FILE SORTER FOR LIFE
-dir *.mov | select BaseName,Extension
+
+# set utf-8 encoding
+$OutputEncoding = New-Object -typename System.Text.UTF8Encoding
+[Console]::OutputEncoding = New-Object -typename System.Text.UTF8Encoding
+
+# set basic dir to sort files in
+New-Variable -Name "BaseDir" -Value "D:\Downloads\"
+$Dirs = "Music", "Pictures", "Movies", "Programs"
+
+foreach ($d in $Dirs) {
+    if (!(Test-Path -Path "$BaseDir$d" )) {
+        New-Item -ItemType directory -Path "$BaseDir$d"
+    }
+}
+
+$movies = "mov", "flv", "mp4", "mkv"
+$audio = "mp3", "ogg", "wma", "flac", "cue", "pls"
+$images = "jpeg", "jpg", "png", "gif", "bmp", "raw"
+$programs = "exe", "msi", "py", "rb", "ps1", "php"
+$extensions = $movies, $audio, $images, $programs
+
+foreach ($ExtList in $extensions) {
+    foreach ($ext in $ExtList) {
+        dir "$BaseDir*.$ext" | select BaseName,Extension
+    }
+}
